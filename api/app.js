482a0a45a62d593e95466d39c8a50b3823b68e74
coutram <coutram@users.noreply.github.com>
2025-05-12 17:46:20 -0400

    get rid of the non prod version

--- api/app.js
+++ api/app.js
@@ -7,14 +7,14 @@ import app from './index.js';
 
 
 // Start the server (only for local development)
-if (process.env.NODE_ENV !== 'production') {
-  try {
-    await app.listen({ port: process.env.PORT })
-    app.log.info(`Server is running on http://localhost:${process.env.PORT}`)
-  } catch (err) {
-    app.log.error(err)
-    process.exit(1)
-  }
+// if (process.env.NODE_ENV !== 'production') {
+//   try {
+//     app.listen({ port: process.env.PORT })
+//     app.log.info(`Server is running on http://localhost:${process.env.PORT}`)
+//   } catch (err) {
+//     app.log.error(err)
+//     process.exit(1)
+//   }
 }
 // start()
 
