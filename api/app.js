a82558b69cdcc2ff86735301a5c6dfa808f67dce
coutram <coutram@users.noreply.github.com>
2025-05-13 08:39:59 -0400

    don't call listen directly

--- api/app.js
+++ api/app.js
@@ -1,22 +1,22 @@
 import app from './index.js';
 
-// Start the server
-// const start = async () => {
-
-// }
-
 
 // Start the server (only for local development)
-// if (process.env.NODE_ENV !== 'production') {
-//   try {
-//     app.listen({ port: process.env.PORT })
-//     app.log.info(`Server is running on http://localhost:${process.env.PORT}`)
-//   } catch (err) {
-//     app.log.error(err)
-//     process.exit(1)
-//   }
-// }
-// start()
+const start = async () => {
+  try {
+    await app.listen({ port: process.env.PORT })
+    app.log.info(`Server is running on http://localhost:${process.env.PORT}`)
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+console.log(process.env.NODE_ENV)
+// Start the server
+if (process.env.NODE_ENV !== 'production') {
+  await start()
+}
 
 export default async function handler(req, reply) {
   await app.ready()
